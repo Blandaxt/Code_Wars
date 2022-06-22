9432b1de919694bb04165bb9e9e24a178ba4c59b
Blandaxt <alainsachevements@gmail.comLafume.alain@gmail.com>
2022-06-22 16:38:25 -0400

    trying to push

--- Codility_BinaryGap/index.js
+++ Codility_BinaryGap/index.js
@@ -17,6 +17,8 @@ Write an efficient algorithm for the following assumptions:
 
 N is an integer within the range [1..2,147,483,647].
 
+does this work?
+
 */
 
 function solution(N) {
@@ -72,5 +74,6 @@ function solution(N) {
     }else{
 
         return binaryGap
+
     }
 }
